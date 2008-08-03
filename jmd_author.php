8b9b71b5bff23f5e0712b0fd48aa8adf4959006a
Jon-Michael Deldin <dev@jmdeldin.com>
2008-08-03 13:47:26 -0600

    Rewritten to use one `display` attribute.

--- jmd_author.php
+++ jmd_author.php
@@ -2,9 +2,8 @@
 $plugin = array(
     'description' => "Provides access to an author's username and email.",
     'type' => 0,
-    'version' => '0.1',
+    'version' => '0.2',
 );
-
 if (!defined('txpinterface')) include_once '../zem_tpl.php';
 
 if (0) {
@@ -12,67 +11,90 @@ if (0) {
 # --- BEGIN PLUGIN HELP ---
 
 h1. jmd_author
-	
+
+"Forum thread":http://forum.textpattern.com/viewtopic.php?id=26080, "hg repo":http://www.bitbucket.org/jmdeldin/jmd_author/
+
 This plugin provides access to an author's username and email address. It can also modify the output of an author's name with case and/or separators.
 
 h2(#attributes). @<txp:jmd_author/>@ attributes
 
-|_. Attribute name |_. Available values |_. Default value |_. Function |
-| @email@ | 1, 0 | 0 | Displays the author's encoded email address |
-| @real_name@ | 1, 0 | 0 | Displays the author's real name |
-| @separator@ | * | " " | Replace spaces in an author's full name with any character |
-| @lowercase@ | 1, 0 | 0 | Outputs lowercase text (username or full name) |
+|_. Attribute |_. Available values |_. Default value |_. Description |
+| @display@ | @email@, @name@, @username@ | @username@ | Outputs an available value |
+| @lowercase@ | @0@, @1@ | @0@ | Lowercases @display@ text |
+| @separator@ | @*@ | @' '@ | Replaces spaces in @display@ with any character |
 
 h2(#examples). Examples
 
 * Username
 ** @<txp:jmd_author/>@
-* Email
-** @<txp:jmd_author email="1"/>@
-* Real name
-** @<txp:jmd_author real_name="1"/>@
-* Lowercase real name, separated with underscores
-** @<txp:jmd_author lowercase="1" real_name="1" separator="_"/>@
+* Link to author profile
+** @<txp:site_url/>staff/<txp:jmd_author display="name" lowercase="1" separator="-"/>@
+* Email address
+** @<txp:jmd_author display="email"/>@
+* Author profiles
+** "TextBook article":http://textpattern.net/wiki/index.php?title=Author_profiles
 
 # --- END PLUGIN HELP ---
-
 <?php
 }
 
 # --- BEGIN PLUGIN CODE ---
 
+if (txpinterface === 'public')
+{
+    global $textarray;
+    $l10n = array(
+        'jmd_author_invalid_display' => 'Invalid display value',
+    );
+    $textarray = array_merge($textarray, $l10n);
+}
+
+/**
+ * Provides access to an author's username and email address.
+ * Replaces the spaces in an author's real name with any character.
+ *
+ * @param array $atts
+ * @param string $atts['display'] 'email', 'name', 'username'
+ * @param bool $atts['lowercase'] Lowercases $display
+ * @param string $atts['separator'] Replace spaces in $display with any char.
+ */
 function jmd_author($atts)
 {
-	global $thisarticle;
-	extract(lAtts(array(
-		'email' => 0,
-		'lowercase' => 0,
-		'separator' => ' ',
-		'real_name' => 0,
-	), $atts));
-	assert_article();
-	$username = $thisarticle['authorid'];
-	$full_name = get_author_name($username);
+    global $thisarticle;
+    extract(lAtts(array(
+        'display' => 'username',
+        'lowercase' => 0,
+        'separator' => ' ',
+    ), $atts));
+    assert_article();
+    $username = $thisarticle['authorid'];
+    $name = get_author_name($username);
+    $out = '';
 
-	if ($email == 1)
-	{
-		$email = eE(safe_field("email", "txp_users", "name='$username'"));
-		$out = $email;
-	}
-	elseif ($real_name == 1)
-	{
-		if ($lowercase == 1)
-		{
-			$full_name = strtolower($full_name);
-		}
-		$out = str_replace(' ', $separator, $full_name);
-	}
-	else
-	{
-		$out = $username;
-	}
-	
-	return $out;
+    switch ($display)
+    {
+        case 'username':
+            $out = $username;
+            break;
+
+        case 'email':
+            $out = eE(safe_field("email", "txp_users", "name='$username'"));
+            break;
+
+        case 'name':
+            $out = get_author_name($username);
+            break;
+
+        default:
+            trigger_error(gTxt('jmd_author_invalid_display'));
+    }
+
+    if ($lowercase == 1)
+    {
+        $out = strtolower($out);
+    }
+
+    return str_replace(' ', $separator, $out);
 }
 
 # --- END PLUGIN CODE ---
