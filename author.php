60411bddab816be308ea86b7936dd73241b900d7
Jon-Michael Deldin <dev@jmdeldin.com>
2009-01-30 09:01:57 -0700

    Update for plugin template

--- author.php
+++ author.php
@@ -1,44 +1,13 @@
 <?php
-$plugin = array(
-    'description' => "Provides access to an author's username and email.",
-    'type' => 0,
-    'version' => '0.2',
-);
-if (!defined('txpinterface')) include_once '../zem_tpl.php';
-
-if (0) {
-?>
-# --- BEGIN PLUGIN HELP ---
-
-h1. jmd_author
-
-"Forum thread":http://forum.textpattern.com/viewtopic.php?id=26080, "hg repo":http://www.bitbucket.org/jmdeldin/jmd_author/
-
-This plugin provides access to an author's username and email address. It can also modify the output of an author's name with case and/or separators.
-
-h2(#attributes). @<txp:jmd_author/>@ attributes
-
-|_. Attribute |_. Available values |_. Default value |_. Description |
-| @display@ | @email@, @name@, @username@ | @username@ | Outputs an available value |
-| @lowercase@ | @0@, @1@ | @0@ | Lowercases @display@ text |
-| @separator@ | @*@ | @' '@ | Replaces spaces in @display@ with any character |
-
-h2(#examples). Examples
-
-* Username
-** @<txp:jmd_author/>@
-* Link to author profile
-** @<txp:site_url/>staff/<txp:jmd_author display="name" lowercase="1" separator="-"/>@
-* Email address
-** @<txp:jmd_author display="email"/>@
-* Author profiles
-** "TextBook article":http://textpattern.net/wiki/index.php?title=Author_profiles
-
-# --- END PLUGIN HELP ---
-<?php
-}
-
-# --- BEGIN PLUGIN CODE ---
+/**
+ * @name            jmd_author
+ * @description     Provides access to an author's username and email.
+ * @author          Jon-Michael Deldin
+ * @author_uri      http://jmdeldin.com
+ * @version         0.2
+ * @type            0
+ * @order           5
+ */
 
 if (txpinterface === 'public')
 {
@@ -97,6 +66,5 @@ function jmd_author($atts)
     return str_replace(' ', $separator, $out);
 }
 
-# --- END PLUGIN CODE ---
-
 ?>
+
